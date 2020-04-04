046f1ffe2b18bd0f7bf7f4bb6b21bd533a0f7d16
ezequielsurace <36217061+ezequielsurace@users.noreply.github.com>
2020-04-04 20:58:13 -0300

    Add pagination

--- app/Traits/ApiResponser.php
+++ app/Traits/ApiResponser.php
@@ -3,7 +3,9 @@
 namespace App\Traits;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Pagination\LengthAwarePaginator;
 use Illuminate\Support\Collection;
+use Illuminate\Support\Facades\Validator;
 
 trait ApiResponser
 {
@@ -19,16 +21,17 @@ trait ApiResponser
 
     protected function showAll(Collection $collection, $code = 200)
     {
-        if($collection->isEmpty()) {
+        if ($collection->isEmpty()) {
             return response()->json(['data' => $collection], $code);
         }
-        
+
         $transformer = $collection->first()->transformer;
 
         $collection = $this->filterData($collection, $transformer);
         $collection = $this->sortData($collection, $transformer);
+        $collection = $this->paginate($collection);
         $collection = $this->transformData($collection, $transformer);
-        
+
         return response()->json($collection, $code);
     }
 
@@ -37,7 +40,7 @@ trait ApiResponser
         $transformer = $instance->transformer;
 
         $instance = $this->transformData($instance, $transformer);
-        
+
         return response()->json($instance, $code);
     }
 
@@ -51,7 +54,7 @@ trait ApiResponser
         foreach (request()->query() as $query => $value) {
             $attribute = $transformer::originalAttribute($query);
 
-            if(isset($attribute, $value)) {
+            if (isset($attribute, $value)) {
                 $collection = $collection->where($attribute, $value);
             }
         }
@@ -61,13 +64,40 @@ trait ApiResponser
 
     protected function sortData(Collection $collection, $transformer)
     {
-        if(request()->has('sort_by')) {
+        if (request()->has('sort_by')) {
             $attribute = $transformer::originalAttribute(request()->sort_by);
 
             $collection = $collection->sortBy($attribute);
         }
 
         return $collection;
+    }
+
+    protected function paginate(Collection $collection)
+    {
+        $rules = [
+            'per_page' => 'integer|min:2|max:50'
+        ];
+
+        Validator::validate(request()->all(), $rules);
+
+        $page = LengthAwarePaginator::resolveCurrentPage();
+
+        $perPage = 15;
+
+        if(request()->has('per_page')) {
+            $perPage = (int) request()->per_page;
+        }
+
+        $results = $collection->slice(($page - 1) * $perPage, $perPage)->values();
+
+        $paginated = new LengthAwarePaginator($results, $collection->count(), $perPage, $page, [
+            'path' => LengthAwarePaginator::resolveCurrentPath(),
+        ]);
+
+        $paginated->appends(request()->all());
+
+        return $paginated;
     }
 
     protected function transformData($data, $transformer)
