5dd639139e748748a8ed114c79560030ef007d35
ezequielsurace <36217061+ezequielsurace@users.noreply.github.com>
2020-04-06 12:15:21 -0300

    Fix problem with the transformation and validation and POST and PUT methods

--- app/Http/Controllers/Product/ProductBuyerTransactionController.php
+++ app/Http/Controllers/Product/ProductBuyerTransactionController.php
@@ -6,11 +6,19 @@ use App\Buyer;
 use App\Http\Controllers\ApiController;
 use App\Product;
 use App\Transaction;
+use App\Transformers\TransactionTransformer;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 
 class ProductBuyerTransactionController extends ApiController
 {
+    public function __construct()
+    {
+        parent::__construct();
+
+        $this->middleware('transform.input:' . TransactionTransformer::class)->only(['store']);
+    }
+    
     /**
      * Store a newly created resource in storage.
      *
